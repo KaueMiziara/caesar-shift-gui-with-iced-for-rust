0338b6ea0f531a6c3ec3bef9b3309b712d4edc72
KaueMiziara <kauemiziara@gmail.com>
2023-01-17 22:46:35 -0300

    Fixed input checker

--- src/caesar_shift.rs
+++ src/caesar_shift.rs
@@ -1,15 +1,15 @@
-use std::{io, ops::Add};
+use std::io;
 
 pub fn cipher(_key: u8) -> String {
     let text = read_input();
 
-    text.trim().to_string().add("rhs")
+    text.trim().to_string()
 }
 
 pub fn decipher(_key: u8) -> String {
     let text = read_input();
 
-    text
+    text.trim().to_string()
 }
 
 
@@ -19,14 +19,21 @@ fn read_input() -> String {
     loop {
         match io::stdin().read_line(&mut buffer) {
             Ok(_) => {
+                let mut alpha: bool = true;
+
                 for char in buffer.trim().chars() {
-                    if !char.is_ascii_alphabetic() {
-                        println!("All characters must be in the latin alphabet.");
+                    if !(char.is_ascii_alphabetic() || char.eq(&' ')) {
+                        println!("All characters should be in the latin alphabet, \
+                        got {char}");
+                        
                         buffer.clear();
+                        alpha = false;
+                        
                         break;
-                    } else {
-                        return buffer;
                     }
+                }
+                if alpha == true {
+                    return buffer;
                 }
             }
             Err(_) => {
