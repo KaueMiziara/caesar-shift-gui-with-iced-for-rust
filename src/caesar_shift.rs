21c8ac0d382f8b8bab7000fd85e5022e1fa4db13
KaueMiziara <kauemiziara@gmail.com>
2023-01-19 15:48:54 -0300

    Added input box to change the shift

--- src/caesar_shift.rs
+++ src/caesar_shift.rs
@@ -1,6 +1,8 @@
 // 'A' in ASCII == 65
 
-pub fn cipher(text: String, shift: u8) -> String {
+pub fn cipher(text: String, shift: String) -> String {
+    let shift = (shift.trim().parse::<u128>().unwrap() % 26) as u8;
+
     text.to_uppercase().chars().map(|char| {
         if char != ' ' {
             (65 + (char as u8 + shift - 65) % 26) as char
@@ -10,10 +12,12 @@ pub fn cipher(text: String, shift: u8) -> String {
     }).collect()
 }
 
-pub fn decipher(text: String, shift: u8) -> String {
+pub fn decipher(text: String, shift: String) -> String {
+    let shift = (shift.trim().parse::<u128>().unwrap() % 26) as u8;
+
     text.to_uppercase().chars().map(|char| {
         if char != ' ' {
-            (65 + (char as u8 - shift + 65) % 26) as char //
+            (65 + (char as u8 - shift + 65) % 26) as char
         } else {
             char
         }
@@ -28,38 +32,39 @@ mod tests {
     // Cipher
     #[test]
     fn lower_case_to_upper() {
-        assert_eq!(cipher(String::from("abc"), 1), "BCD");
+        assert_eq!(cipher(String::from("abc"), "1".to_string()), "BCD");
     }
 
     #[test]
     fn string_with_space_works() {
-        assert_eq!(cipher(String::from("attack the castle"), 1), "BUUBDL UIF DBTUMF");
+        assert_eq!(cipher(String::from("attack the castle"), "1".to_string()),
+                   "BUUBDL UIF DBTUMF");
     }
 
     #[test]
     fn rotates_back_to_start() {
-        assert_eq!(cipher(String::from("z"), 1), "A");
+        assert_eq!(cipher(String::from("z"), "1".to_string()), "A");
     }
 
     #[test]
     fn key_goes_back_to_zero_cipher() {
-        assert_eq!(cipher(String::from("A"), 26), "A");
-        assert_eq!(cipher(String::from("A"), 27), "B");
-        assert_eq!(cipher(String::from("Z"), 26), "Z");
-        assert_eq!(cipher(String::from("Z"), 27), "A");
+        assert_eq!(cipher(String::from("A"), "26".to_string()), "A");
+        assert_eq!(cipher(String::from("A"), "27".to_string()), "B");
+        assert_eq!(cipher(String::from("Z"), "26".to_string()), "Z");
+        assert_eq!(cipher(String::from("Z"), "27".to_string()), "A");
     }
 
     // Decipher
     #[test]
     fn rotates_to_end() {
-        assert_eq!(decipher(String::from("A"), 1), "Z");
+        assert_eq!(decipher(String::from("A"), "1".to_string()), "Z");
     }
 
     #[test]
     fn key_goes_back_to_zero_decipher() {
-        assert_eq!(decipher(String::from("A"), 26), "A");
-        assert_eq!(decipher(String::from("A"), 27), "Z");
-        assert_eq!(decipher(String::from("Z"), 26), "Z");
-        assert_eq!(decipher(String::from("Z"), 27), "Y");
+        assert_eq!(decipher(String::from("A"), "26".to_string()), "A");
+        assert_eq!(decipher(String::from("A"), "27".to_string()), "Z");
+        assert_eq!(decipher(String::from("Z"), "26".to_string()), "Z");
+        assert_eq!(decipher(String::from("Z"), "27".to_string()), "Y");
     }
 }
--- src/gui.rs
+++ src/gui.rs
@@ -8,11 +8,13 @@ use iced::widget::{button, column, container, row, text, text_input};
 pub struct Scene {
     input_text: String,
     output_text: String,
+    shift: String,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     InputChanged(String),
+    ShiftChanged(String),
     EncryptPressed,
 }
 
@@ -24,6 +26,19 @@ impl Scene {
         for char in self.input_text.trim().chars() {
             if !(char.is_ascii_alphabetic() || char.eq(&' ')) {
                 self.input_text.clear();
+                valid = false;
+                break;
+            }
+        }
+        valid
+    }
+
+    fn shift_is_valid(&mut self) -> bool {
+        let mut valid: bool = true;
+
+        for char in self.shift.trim().chars() {
+            if !(char.is_numeric()) {
+                self.shift.clear();
                 valid = false;
                 break;
             }
@@ -58,20 +73,29 @@ impl Sandbox for Scene {
             },
             Message::EncryptPressed => {
                 if self.input_is_valid() {
-                    self.output_text = cipher(self.input_text.clone(), 2);
+                    self.output_text = cipher(self.input_text.clone(),
+                                              self.shift.clone());
                     self.input_text = String::from("");
                 } else {
                     self.output_text = 
                         String::from("All characters should be in the latin alphabet")
                 }
             },
+            Message::ShiftChanged(shift) => {
+                if self.shift_is_valid() {
+                    self.shift = shift;
+                } else {
+                    self.output_text = 
+                        String::from("The shift value should be a number.")
+                }
+            }
         }
     }
 
 
     // TODO decrypt button
     fn view(&self) -> iced::Element<Message> {
-        let text_input = text_input(
+        let user_input = text_input(
             "Text to encrypt...",
             &self.input_text,
             Message::InputChanged
@@ -79,17 +103,28 @@ impl Sandbox for Scene {
             .padding(10)
             .size(20);
 
+
+        let shift_input = text_input(
+            "Shift...",
+            &self.shift,
+            Message::ShiftChanged
+        )
+            .padding(10)
+            .size(20)
+            .width(Length::Units(80));
         
-        let button =
+
+        let encrypt_button =
             button("Encrypt")
             .padding(10)
             .on_press(Message::EncryptPressed);
 
-
+        
         let content = column![
             row![
-                text_input,
-                button,
+                user_input,
+                shift_input,
+                encrypt_button,
             ]
             .spacing(10)
             .width(Length::Units(800))
